e103748ec462b799d8e3c6998b2462c4ddf37773
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-08 15:35:54 -0600

    props and styles

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -12,10 +12,12 @@ const getSum = (a, b) => {
     return a + b;
 }
 
-export const FirstApp = () => { // Functional component
+export const FirstApp = ({title, subTitle = 0}) => { // Functional component
+    console.log(title);
   return (
     <> {/* Or we can use FRAGMENT of React */}
-        <h1>{ getMessage() } ({getSum(2,3)})</h1>
+        <h1>-{ title } ({subTitle+1})</h1>
+        <h2>{ getMessage() } ({getSum(2,3)})</h2>
         <code>{ JSON.stringify(newMessage) }</code> {/* Could not be an object */}
         <p>Soy un subtitulo ({ newMessage.age })</p>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import { FirstApp } from './FirstApp';
 
+import './styles.css'; // Import styles0
+
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp title="Hola, soy aldo" subTitle={ 123 } />
     </React.StrictMode>
 )
