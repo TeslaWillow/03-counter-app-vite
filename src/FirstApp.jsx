219254e4b2cbd94db8efdaf97894c400c25d651e
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-09 14:53:59 -0600

    test on component.

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -22,9 +22,10 @@ export const FirstApp = ({title, subTitle}) => { // Functional component
 
   return (
     <> {/* Or we can use FRAGMENT of React */}
-        <h1>{ title }</h1>
+        <h3 data-testid="test-title">{ title } </h3>
         <h2>{ getMessage() } ({getSum(2,3)})</h2>
         <code>{ JSON.stringify(newMessage) }</code> {/* Could not be an object */}
+        <p>{subTitle}</p>
         <p>{subTitle}</p>
         <p>Soy un subtitulo ({ newMessage.age })</p>
     </>
--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -2,8 +2,38 @@ import { render } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas en <FirstApp />', () => { 
-    test('Debe hacer match con el snapshot', () => {
+    /**
+     * test('Debe hacer match con el snapshot', () => {
         const title = 'Hola, soy hector';
-        render(<FirstApp title={title} />);
+        const {container} = render(<FirstApp title={title} />);
+
+        expect(container).toMatchSnapshot();
+    });
+     */
+
+    test('Debe mostrar el titulo en un h1', () => {
+        const title = 'Hola, soy hector';
+        const { container, getByText, getByTestId } = render(<FirstApp title={title} />);
+
+        expect( getByText(title) ).toBeTruthy();
+
+        // This is too strict
+        // const h1 = container.querySelector("h1");
+        // expect(h1.innerHTML).toContain(title); // Diferent because dont care blank spaces
+
+        expect( getByTestId('test-title').innerHTML ).toContain(title);
+    });
+
+    test('Debe de mostrar el subtitulo enviado por props', () => {
+        const title = 'Hola, soy hector';
+        const subTitle = 123;
+        const {  getAllByText } = render(
+            <FirstApp 
+                title={title} 
+                subTitle={subTitle} 
+            />
+        );
+
+        expect( getAllByText(subTitle).length ).toBe(2);
     });
  })
